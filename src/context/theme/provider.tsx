e5009a388e20feaf1340bac46f557c0ce2d5f211
Karl-Johan Bailey <karl@cenedex.com>
2023-09-24 15:47:05 -0400

    fix: remove isMount check in theme provider

--- src/context/theme/provider.tsx
+++ src/context/theme/provider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext } from "react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "./config";
 import useDarkMode from "use-dark-mode";
@@ -19,12 +19,6 @@ export default function Provider({
 
     const darkmode = useDarkMode(true);
     const theme = darkmode.value ? darkTheme : lightTheme
-
-    const [isMounted, setIsMounted] = useState(false)
-
-    useEffect(() => {
-      setIsMounted(true)
-    }, [])
 
     return (
       <DarkModeContext.Provider value={darkmode}>
